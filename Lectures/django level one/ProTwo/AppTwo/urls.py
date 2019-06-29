948caa6caa63c83ad086969969d70ab1e7758bcf
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-06-29 15:16:13 +0100

    ProTwo: Added view and template for fake data

--- Lectures/django level one/ProTwo/AppTwo/urls.py
+++ Lectures/django level one/ProTwo/AppTwo/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from AppTwo import views
 
 urlpatterns = [
+    url(r'^users', views.users_page, name='users'),
     url(r'^index', views.index_page, name='index'),
     url(r'^help/', views.help_page, name="help")
 ]
--- Lectures/django level one/ProTwo/AppTwo/views.py
+++ Lectures/django level one/ProTwo/AppTwo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import Users
 
 def index_page(request):
     return HttpResponse(r"<em>My second app </em>")
@@ -7,3 +8,8 @@ def index_page(request):
 def help_page(request):
     mappings = {"help_txt": "How can I help you today?"}
     return render(request, 'AppTwo/help.html', context=mappings)
+
+def users_page(request):
+    users_list = Users.objects.order_by('id')
+    users_dict = {'users': users_list}
+    return render(request, 'AppTwo/users.html', context=users_dict)
