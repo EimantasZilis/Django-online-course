34ab5817a23ea1b697b0a90475945e85d2e19fb1
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-06-29 12:28:15 +0100

    Created new models for first_app

--- Lectures/django level one/first_project/first_project/urls.py
+++ Lectures/django level one/first_project/first_project/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 from first_app import views
 
 urlpatterns = [
-    path(r'^$', views.index, name='index'),
+    path(r'', views.index, name='index'),
     path(r'first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
