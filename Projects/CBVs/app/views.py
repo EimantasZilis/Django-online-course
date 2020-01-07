74a3322f962e0848564576b138ba9c04a2e420a0
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-07 16:29:08 +0000

    Simple CBV

--- Projects/CBVs/app/views.py
+++ Projects/CBVs/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
-def index(request):
-    return render(request, "app/index.html")
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("CBVs man")
+
+# def index(request):
+#     return render(request, "app/index.html")
 
