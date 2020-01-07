3bd7ffeb1ae30ff60a8449f3f17c9891af5b052c
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-07 12:07:31 +0000

    User registration forms
    Create registration forms that extend the existing Django's User
    model. Also store images uploaded by the user in the media/profile_pics
    directory.

--- Projects/user_authentication/app/models.py
+++ Projects/user_authentication/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfileInfo(models.Model):
+    # Associate new model with User class
+    # Any extra attributes will be extending user class
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Upload to specified output directory within media folder.
+    # 'profile_pics'  needs to exist
+    profile_pic = models.ImageField(upload_to="profile_pics", blank=True)
+    portfolio_site = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
