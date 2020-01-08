4cbafd524c28ddb7cb76b29e46e473973c338fef
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-08 15:31:54 +0000

    Setup blog models.py

--- Projects/web/blog/models.py
+++ Projects/web/blog/models.py
@@ -1,3 +1,48 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+
+class Post(models.Model):
+    # Author related to superuser of the website
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    publication_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publication_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        # When you click post, where should it go next?
+        # This method redirects after successful 'post' publication.
+        # To make this work, the method name must be explictly set
+        # to 'get_absolute_url' for django to find it
+        return reverse("post_detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.text
