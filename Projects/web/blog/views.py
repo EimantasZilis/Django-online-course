b9358c0593655e7d0c28f5ec4c83b5aeee452345
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-09 09:11:18 +0000

    More views and URLs for post and comments
    Function views added to add, remove and approve comments

--- Projects/web/blog/views.py
+++ Projects/web/blog/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -66,5 +68,40 @@ class DraftListView(LoginRequiredMixin, ListView):
         return Post.objects.filter(
             published_date__isnull=True
         ).order_by('created_date')
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/comment_form.html", {"form": form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect("post_detail", pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect("post_detail", pk=post_pk)
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect("post_detail", pk=pk)
 
 
