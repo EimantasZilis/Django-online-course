efd4f5e37d9859d6de8597af36d63ddb5f6c9f0a
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-13 19:25:28 +0000

    Added basic homepage

--- Projects/star_social/star_social/urls.py
+++ Projects/star_social/star_social/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from star_social import views
 
 urlpatterns = [
+    path('', views.HomePage.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
