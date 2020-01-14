96f8197eaa10d40ecc6cfc51fa43759c763e78aa
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-14 13:44:28 +0000

    Created templates and views for creating, listing and viewing groups

--- Projects/star_social/groups/views.py
+++ Projects/star_social/groups/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from groups.models import Group, GroupMember
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    # Users shouldn't be able to edit fields like slug or description_html
+    fields = ("name", "description")
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    # For viewing single group
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    # For viewing multiple groups
+    model = Group
