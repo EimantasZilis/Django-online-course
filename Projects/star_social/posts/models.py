f50d0f5d51e5274a334a2e67ff6708ead70fbcbf
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-14 07:10:21 +0000

    Defined Group and post models

--- Projects/star_social/posts/models.py
+++ Projects/star_social/posts/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+import misaka
 
-# Create your models here.
+from groups.models import Group
+
+
+class Post(models.Model):
+    User = models.ForeignKey(User, related_name="posts", on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name="posts", null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("posts:single", kwargs={
+            "username": self.user.username,
+            "pk": self.pk
+            }
+        )
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message']
+
+
+User = get_user_model()
