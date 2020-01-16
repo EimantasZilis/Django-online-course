706e89bd8af78f6c9b207f49f9fc2de39ab1ff39
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-16 08:56:32 +0000

    Added Join/leave group views

--- Projects/star_social/groups/views.py
+++ Projects/star_social/groups/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
@@ -20,3 +21,35 @@ class SingleGroup(generic.DetailView):
 class ListGroups(generic.ListView):
     # For viewing multiple groups
     model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get("slug"))
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request, ("Warning, already a member"))
+        else:
+            messages.success(self.request, ("You are now a member"))
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user, group__slug=self.kwargs.get("slug")
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, "Sorry, you are not in this group!")
+        else:
+            membership.delete()
+            messages.success(self.request, "You have left the group!")
+        return super().get(request, *args, *kwargs)
