f0dc4981b8e9d7a93dbfa2337e08dec1c43281d7
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-16 09:04:40 +0000

    Fixed typos and minor bugs

--- Projects/star_social/groups/admin.py
+++ Projects/star_social/groups/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from groups import models
 
+
 class GroupMemberInline(admin.TabularInline):
     model = models.GroupMember
 
-admin.site.regisrter(models.Group)
 
+admin.site.register(models.Group)
--- Projects/star_social/posts/models.py
+++ Projects/star_social/posts/models.py
@@ -6,13 +6,17 @@ import misaka
 
 from groups.models import Group
 
+User = get_user_model()
+
 
 class Post(models.Model):
-    User = models.ForeignKey(User, related_name="posts", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
-    group = models.ForeignKey(Group, related_name="posts", null=True, blank=True, on_delete=models.CASCADE)
+    group = models.ForeignKey(
+        Group, related_name="posts", null=True, blank=True, on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.message
@@ -22,15 +26,10 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse("posts:single", kwargs={
-            "username": self.user.username,
-            "pk": self.pk
-            }
+        return reverse(
+            "posts:single", kwargs={"username": self.user.username, "pk": self.pk}
         )
 
     class Meta:
-        ordering = ['-created_at']
-        unique_together = ['user', 'message']
-
-
-User = get_user_model()
+        ordering = ["-created_at"]
+        unique_together = ["user", "message"]
