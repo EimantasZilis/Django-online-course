4019d015618bd23599585b1d17cb24299d567fc4
EimantasZilis <zilis.eimantas@gmail.com>
2020-01-20 08:41:54 +0000

    Fixed settings.py to locate all static files

--- Projects/star_social/star_social/settings.py
+++ Projects/star_social/star_social/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR= [Path(BASE_DIR) / "static"]
+STATICFILES_DIRS = [Path(BASE_DIR) / "static"]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
