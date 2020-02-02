7ab6ad65e3b82d3928f3fea9f7f95442e54947e4
EimantasZilis <zilis.eimantas@gmail.com>
2020-02-02 16:01:17 +0000

    debug toolbar

--- Projects/ProTwo/ProTwo/settings.py
+++ Projects/ProTwo/ProTwo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AppTwo'
+    'debug_toolbar',
+    'AppTwo',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'ProTwo.urls'
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = ['127.0.0.1']
--- Projects/ProTwo/ProTwo/urls.py
+++ Projects/ProTwo/ProTwo/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
 
 urlpatterns = [
     path(r'AppTwo/', include('AppTwo.urls')),
     path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
