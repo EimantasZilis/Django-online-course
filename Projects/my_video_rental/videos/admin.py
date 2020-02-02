6c5384edff30efd626b330b15905a1be564cb3f2
EimantasZilis <zilis.eimantas@gmail.com>
2020-02-02 17:08:49 +0000

    Add more fields visible in admin record view

--- Projects/my_video_rental/videos/admin.py
+++ Projects/my_video_rental/videos/admin.py
@@ -5,6 +5,7 @@ class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
     search_fields = ['title', 'length']
     list_filter = ['release_year', 'title']
+    list_display = ['title', 'release_year', 'length']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
