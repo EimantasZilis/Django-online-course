224bb2253b496532dfb34eb915621ad546e7abab
EimantasZilis <zilis.eimantas@gmail.com>
2020-02-02 17:04:32 +0000

    Add filters to model attributes in admin

--- Projects/my_video_rental/videos/admin.py
+++ Projects/my_video_rental/videos/admin.py
@@ -4,6 +4,7 @@ from videos import models
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
     search_fields = ['title', 'length']
+    list_filter = ['release_year', 'title']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
