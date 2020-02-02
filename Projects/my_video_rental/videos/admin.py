1c9d996c1967683f940bffb7177565af241c47ad
EimantasZilis <zilis.eimantas@gmail.com>
2020-02-02 16:57:49 +0000

    Search field in django admin model

--- Projects/my_video_rental/videos/admin.py
+++ Projects/my_video_rental/videos/admin.py
@@ -3,6 +3,7 @@ from videos import models
 
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
+    search_fields = ['title']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
