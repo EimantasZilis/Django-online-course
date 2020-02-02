d484be774cfb1829b9df99e58453ee4331734483
EimantasZilis <zilis.eimantas@gmail.com>
2020-02-02 17:01:09 +0000

    Added django admin search in model

--- Projects/my_video_rental/videos/admin.py
+++ Projects/my_video_rental/videos/admin.py
@@ -3,7 +3,7 @@ from videos import models
 
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
-    search_fields = ['title']
+    search_fields = ['title', 'length']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
